858a8f2f0b3452bb1b7a22c23d67cac4284e0ea0
marwolaethblack <a.unal677@gmail.com>
2018-04-20 11:55:45 +0200

    block while playing text to speech

--- modules/tts.py
+++ modules/tts.py
@@ -1,9 +1,17 @@
 from gtts import gTTS
 import vlc
+import time
 
 
 
 def say(text):
     tts = gTTS(text=text, lang='en')
     tts.save("tts.mp3")
-    vlc.MediaPlayer('./tts.mp3').play()
+    stream = vlc.MediaPlayer('./tts.mp3')
+    stream.play()
+
+    #block while playing the text to speech sound file so the voice recognition doesnt capture it
+    while str(stream.get_state()) != "State.Ended":
+        time.sleep(1)
+    
+    
